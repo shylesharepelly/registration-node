b1c2a99a84e03ca6846d523c4571997bbe3e698b
shylesharepelly <shylesharepelly@gmail.com>
2022-10-04 12:49:47 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
-const hello = () => {
-    console.log("Hello Node.js!");
-};
+const http = require("http");
+const fs = require("fs");
 
-hello();
+let homeContent = "";
+let projectContent = "";
+let registrationcontent="";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+      throw err;
+    }
+    registrationContent = registration;
+  });
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(5000);
+
